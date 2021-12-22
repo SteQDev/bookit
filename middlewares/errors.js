d8f62ca909f353876e7f45839e7f95e1ac7a7af2
Stephen Quigley <you@example.com>
2021-12-22 10:01:15 +0000

    copied errors

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -4,20 +4,21 @@ export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
 
   let error = { ...err };
+
   error.message = err.message;
 
-  /// Wrong Mongoose Object ID Error Message
-
-  if (err.name == "CastError") {
-    const message = `Resourse not found. Invalid: ${err.path}`;
+  // Wrong Mongoose Object ID Error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
     error = new ErrorHandler(message, 400);
   }
 
-  // Handling mongoose validation errors
-  if (err.name == "ValidationError") {
+  // Handling mongoose Validation error
+  if (err.name === "ValidationError") {
     const message = Object.values(err.errors).map((value) => value.message);
     error = new ErrorHandler(message, 400);
   }
+
   res.status(err.statusCode).json({
     success: false,
     error,
